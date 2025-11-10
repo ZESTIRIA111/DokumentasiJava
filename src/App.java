424f6a48ab1c2040a2bb534151739589519f6926
Akbar Syadzwana <akbarsyadzwana@gmail.com>
2025-11-10 22:04:57 +0700

    Menambahkan file dokumentasi JavaDocs

---
+++ src/App.java
@@ -0,0 +1,18 @@
+/**
+ * Program utama untuk latihan membuat dokumentasi JavaDocs.
+ * <p>Program ini hanya menampilkan pesan sederhana ke konsol.</p>
+ *
+ * @author Akbar
+ * @version 1.0
+ */
+public class App {
+
+    /**
+     * Titik masuk utama program.
+     *
+     * @param args argumen baris perintah
+     */
+    public static void main(String[] args) {
+        System.out.println("Halo dunia dari JavaDocs!");
+    }
+}
